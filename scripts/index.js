79c48baafcda2c679e20589a2798b47aac564afc
Deepak Agarwal <deepak710agarwal@gmail.com>
2022-02-21 10:32:01 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,56 @@
-const wrapper = document.querySelector('.wrapper');
-const indicators = [...document.querySelectorAll('.indicators button')];
+function validate() {  
+    var result = ""; 
+	result += validateName (); 
+	result += validateEmail ();
+	result += validatePhoneNumber();
+	
+	if (result == "") {
+        alert(" Your details has stored successfully. We will contact you shortly.");
+        return true;
+    }
+    alert("Please enter valid input in all text box.");
+	return false;	
+}
 
-let currentTestimonial = 0; // Default 0
+// function to get the value of inputs entered by user.
+function valueOf(name) {
+	return document.getElementsByName(name)[0].value;
+}
 
-indicators.forEach((item, i) => {
-    item.addEventListener('click', () => {
-        indicators[currentTestimonial].classList.remove('active');
-        wrapper.style.marginLeft = `-${100 * i}%`;
-        item.classList.add('active');
-        currentTestimonial = i;
-    })
-})
+function validateEmail() {
+    var email = valueOf("email");                                 
+    var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;    
+
+    if (reg.test(email)) {
+        document.getElementById("invalidEmail").innerHTML="";
+        return "";
+    }
+        document.getElementById("invalidEmail").innerHTML="Error: This email is not valid.";
+        return "error";
+}
+
+function validateName() {
+    var firstname = valueOf("name"); 
+    var reg = /^[A-Za-z]+$/;          
+	if (!reg.test(firstname)) {
+        document.getElementById("invalidName").innerHTML="Error: The first Name should not contain any numbers.";
+        return "error";
+    }	
+    document.getElementById("invalidName").innerHTML="";
+    return "";
+}
+
+
+function validatePhoneNumber() {
+    var password = valueOf("phone");     
+    var reg = /^[A-Za-z]+$/;    
+    if (reg.test(password)) {
+        document.getElementById("invalidPhoneNumber").innerHTML="Error: Phone number should only contain numeric characters.\n";
+        return "error";
+    }
+    else if (password.length < 10 || password.length > 10 ) {
+        document.getElementById("invalidPhoneNumber").innerHTML="Error: Phone number should be 10 charater long.\n";
+		return "error";
+    }
+    return "";
+}
